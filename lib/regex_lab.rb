7da82d9c549062f8ed6272d7d7079c9c4593440e
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-22 04:26:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z].+[\.!?]$/) then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
