ea328bb1df0078c348d53a511969f09c8bdb1943
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-22 04:28:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,9 @@ end
 
 
 def valid_phone_number?(phone)
-
+if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
+    return true
+  else
+    return false
+  end
 end
